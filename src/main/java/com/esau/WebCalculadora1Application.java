62a2293f268209b9fe27b1caa5162548525347f6
Esau <Esau@DESKTOP-I42A1U3>
2022-05-18 16:25:06 -0500

    Calculadora

---
+++ src/main/java/com/esau/WebCalculadora1Application.java
@@ -0,0 +1,13 @@
+package com.esau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalculadora1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCalculadora1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/esau/WebCalculadora1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculadora1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
